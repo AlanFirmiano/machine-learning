fd358f7106675f6f68228c2a227633ea833e466b
alanfirmiano <alanfirmiano18@gmail.com>
2018-07-13 22:23:44 -0300

    taxa de acerto

--- classificacao.py
+++ classificacao.py
@@ -17,6 +17,15 @@ modelo.fit(dados,marcacoes)
 
 misterioso1 = [1,1,1]
 misterioso2 = [1,0,0]
+misterioso3 = [0,0,1]
 
-teste = [misterioso1,misterioso2]
-print(modelo.predict(teste))
+teste = [misterioso1,misterioso2,misterioso3]
+marcacoes_teste = [-1,1,1]
+
+resultado = modelo.predict(teste)
+print(resultado)
+diferenca = (resultado - marcacoes_teste)
+acertos = [d for d in diferenca if d == 0]
+
+taxa = 100.0 * len(acertos) / len(teste)
+print(taxa)
